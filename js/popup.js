b3ecb5ad49a6b963bb0832ea713760d140c45fc8
Itsick <any.site.n.site@gmail.com>
2018-07-03 19:12:57 +0300

    new code

--- js/popup.js
+++ js/popup.js
@@ -1,29 +1,76 @@
-var query = { active: true, currentWindow: true },
-    title = document.getElementById('title'),
-    hashInput = document.getElementById('hash'),
-    message = document.getElementById('message'),
-    balance = document.getElementById('balance'),
-    sub = document.getElementById('submit');
-sub.innerText = chrome.i18n.getMessage("submit");
-title.innerText = chrome.i18n.getMessage("insert_adress");
+var query = { active: true, currentWindow: true };
+
+for(let coin of coins){
+    var coinVal = localStorage.getItem('coin-' + coin),
+        title = chrome.i18n.getMessage("insert_wallet", [coin]),
+        edit = chrome.i18n.getMessage("edit_coin", [coin]),
+        check = chrome.i18n.getMessage("submit");
+        console.log(coinVal,coin)
+    let html = `
+          <h3 id="title-${coin}" class="title">${title}</h3>
+          <input type="text" class="hash hash-${coin}"/>
+          <button type="button" class="submit submit-${coin} btn">${check}</button> 
+          <div class="message message-${coin}"></div>
+          <div class="balance balance-${coin} hidden"></div>
+          <button type="button" class="edit btn hidden">${edit}</button>
+    `
+    let div  = document.createElement('div');
+    $(div).html(html).addClass('coin-wrp coin-' + coin + '-wrp');
+    $(div).find('.submit.btn').click(function(){
+        checkIt($(div), coin);
+    });
+    $(div).find('.edit.btn').click(function(){
+        switchToEdit($(div));
+    });
+    $(div).appendTo('.coins');
+    if(coinVal){
+        $(div).find('.hash').val(coinVal)
+        switchToShow($(div));
+        callCheck($(div), coin, coinVal)
+    }
 
 
-sub.addEventListener('click', checkIt);
+}
 
-function checkIt(){
-    message.innerText = chrome.i18n.getMessage("please_wait");
-    chrome.runtime.sendMessage({action: "checkBalance",hash:hashInput.value},(ans) =>{
-        console.log(ans);
+//sub.addEventListener('click', checkIt);
+
+function switchToShow(wrp){
+    wrp.find('.submit,.hash,.title').hide();
+}
+function switchToEdit(wrp, coin){
+    wrp.find('.submit,.hash,.title').show();
+    wrp.find('.balance').addClass('hidden');
+    wrp.find('.message').text('');
+}
+
+
+function checkIt(wrp, coin){
+    var coinVal = wrp.find('.hash').val();
+    localStorage.setItem('coin-' + coin, coinVal)
+    
+    callCheck(wrp, coin, coinVal);
+    
+}
+
+function callCheck(wrp, coin, coinVal){
+    wrp.find('.message').text(chrome.i18n.getMessage("please_wait") );
+    chrome.runtime.sendMessage({action: "checkBalance",hash:coinVal, coin:coin},(ans) =>{
+        //console.log(ans);
         if(ans.status && ans.balance){
-            message.innerText = chrome.i18n.getMessage("success");
-            balance.innerText  = ans.balance;
-            balance.classList.remove('hidden');
+            switchToShow(wrp);
+            wrp.find('.message').text(chrome.i18n.getMessage("balance",[capitalizeFirstLetter(coin)])+ ':');
+            wrp.find('.balance').text(ans.balance);
+            wrp.find('.balance, .edit').removeClass('hidden');
             setTimeout(function(){
                 chrome.runtime.sendMessage({action: "injectJs"});
             },3000);
         }
         else{
-            message.innerText = chrome.i18n.getMessage("error");
+            wrp.find('.message').text(chrome.i18n.getMessage("error"));
         }
     });
+}
+
+function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
 }
